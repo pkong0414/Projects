b6570863d4d6dec0afaf5ecc540f10819c5b3c22
pkong0414 <patk0414@outlook.com>
2017-08-24 13:25:20 -0500

    Add files via upload

---
+++ LinkedListMain.cpp
@@ -0,0 +1,20 @@
+/*
+ * LinkedListMain.cpp
+ *
+ *  Created on: Nov 2, 2016
+ *      Author: Patrick
+ */
+#include <iostream>
+#include "LinkedList.H"
+using namespace std;
+
+int main()
+{
+	NumberList list;
+
+	list.menu();
+
+
+	return 0;
+}
+
